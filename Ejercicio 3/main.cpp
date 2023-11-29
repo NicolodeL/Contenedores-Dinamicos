2c775c82a26a9a8c151c52f96024e584694bbcd4
NlodeL <nlodelop@myuax.com>
2023-11-29 20:54:13 +0100

    cambio un par de detalles del código y acabo con el apartado

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -5,14 +5,11 @@ using namespace std;
 
 int main() {
 vector<int> numeros = {5,9,3,4,1,6,20};
-std::sort(numeros.begin(), numeros.end());
-std::cout << "Elementos del vector despues de ordenar: ";
+sort(numeros.begin(), numeros.end());
+cout << "Elementos del vector despues de ordenar: ";
 for (int num : numeros) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
 int maxNum = *std::max_element(numeros.begin(), numeros.end());
-std::cout << "\nEl numero mas grande es: " << maxNum << std::endl;
-
-
-
+cout << "\nEl numero mas grande es: " << maxNum << endl;
 }
