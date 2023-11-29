c7d12bd7570aa9b58306be372baa0aefdfa48b2b
NlodeL <nlodelop@myuax.com>
2023-11-29 20:50:08 +0100

    muestro los valores ordenados de menor a mayor

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main() {
 vector<int> numeros = {5,9,3,4,1,6,20};
 std::sort(numeros.begin(), numeros.end());
+    std::cout << "Elementos del vector despues de ordenar: ";
+    for (int num : numeros) {
+        std::cout << num << " ";
+    }
 
 
 
