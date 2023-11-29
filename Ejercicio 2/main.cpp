cc8fffcc476933301e5a02d060d38e015328ff05
NlodeL <nlodelop@myuax.com>
2023-11-29 20:29:05 +0100

    ayudandome de github copilot muestro cómo funcionan las operaciones push, pop y top en la pila

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main () {
     list<double> decimales = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10};
     stack<double, std::list<double>> pila(decimales);
+    cout << "Elemento superior de la pila: " << pila.top() << std::endl;
+    pila.push(6.6);
+    std::cout << "Nuevo elemento superior de la pila: " << pila.top() << std::endl;
+    pila.pop();
+    std::cout << "Elemento superior de la pila después de pop: " << pila.top() << std::endl;
 
     return 0;
 }
