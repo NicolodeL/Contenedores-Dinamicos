090544b4db8766331ba0f2e79f1a26daeeb89cac
NlodeL <nlodelop@myuax.com>
2023-11-29 20:24:51 +0100

    Primero creo la lista que utilizaré en el ejercicio

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <list>
 #include <stack>
-
+using namespace std;    
 
 int main () {
-std::cout << "Hello, World!" << std::endl;
+    list<double> decimales = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10};
 
     return 0;
 }
