27a9be29a9258fbce0cd5f14036e6e1e81cb5ce4
NlodeL <nlodelop@myuax.com>
2023-11-29 20:53:25 +0100

    añado una funcion que dice el numero más grande de la lista de vectores ordenada

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main() {
 vector<int> numeros = {5,9,3,4,1,6,20};
 std::sort(numeros.begin(), numeros.end());
-    std::cout << "Elementos del vector despues de ordenar: ";
-    for (int num : numeros) {
+std::cout << "Elementos del vector despues de ordenar: ";
+for (int num : numeros) {
         std::cout << num << " ";
     }
+int maxNum = *std::max_element(numeros.begin(), numeros.end());
+std::cout << "\nEl numero mas grande es: " << maxNum << std::endl;
 
 
 
