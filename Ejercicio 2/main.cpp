5085f4f0025c907cf07a4e35fcc5267891621d12
NlodeL <nlodelop@myuax.com>
2023-11-29 20:26:44 +0100

    creo un adaptador de secuencia stack

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <list>
 #include <stack>
-using namespace std;    
+using namespace std;
 
 int main () {
     list<double> decimales = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10};
+    stack<double, std::list<double>> pila(decimales);
 
     return 0;
 }
