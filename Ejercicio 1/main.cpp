0d0c668ad65dd1eacd0ffd56de688b7092d40096
NlodeL <nlodelop@myuax.com>
2023-11-29 19:43:14 +0100

    incluyo los vectores y creo el vector con el que trabajaré

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,9 +1,11 @@
-//
-// Created by nicol on 29/11/2023.
-//
+
 #include <iostream>
+#include <vector>
+
 using namespace std;
 int main() {
+    vector <int> coches; {1,2,3,4,5,6,7,8,9,10};
+    
     cout << "Hello, World!" << endl;
 
     return 0;
