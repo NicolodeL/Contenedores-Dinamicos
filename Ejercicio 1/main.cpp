be7c2fae2f4aaadc048d73091b2a0f682c4f3f1a
NlodeL <nlodelop@myuax.com>
2023-11-29 19:50:05 +0100

    encuentro la forma correcta de recorrer el vector y la dejo estar

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 int main() {
     vector <int> coches {1,2,3,4,5,6,7,8,9,10};
-    for (int i = 0; i < coches.size(); i++) {
-        cout << coches[i] << endl;
-    }
 
     for(std::vector<int>::iterator it = coches.begin(); it != coches.end(); ++it) {
         std::cout << *it << std::endl;
