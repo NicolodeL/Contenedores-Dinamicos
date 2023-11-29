f469680928b60437a59fa2909f4b49b008e2384c
NlodeL <nlodelop@myuax.com>
2023-11-29 20:36:50 +0100

    cambio algunos detalles y termino el ejercicio

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 
 int main () {
-    list<double> decimales = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10};
+    list<double> decimales = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9};
+
     stack<double, std::list<double>> pila(decimales);
-    cout << "Elemento superior de la pila: " << pila.top() << std::endl;
+
+
+    cout << "Elemento superior de la pila: " << pila.top() << endl;
+
     pila.push(6.6);
-    std::cout << "Nuevo elemento superior de la pila: " << pila.top() << std::endl;
+    cout << "Nuevo elemento superior de la pila: " << pila.top() << endl;
+
     pila.pop();
-    std::cout << "Elemento superior de la pila después de pop: " << pila.top() << std::endl;
+    cout << "Elemento superior de la pila despues de pop: " << pila.top() << endl;
 
     return 0;
 }
