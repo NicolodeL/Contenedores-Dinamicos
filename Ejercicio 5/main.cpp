df762e9aca565072a36b8f61621c2b62d1920b8d
NlodeL <nlodelop@myuax.com>
2023-11-30 16:08:44 +0100

    modifico la expresion anterior para que mediante el uso de punteros vaya operando cada uno de los elementos del vector

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -9,8 +9,8 @@ int main() {
     auto cuadrado = [](int num) { return num * num; };
     cout << cuadrado(5) << endl;
     vector<int> numeros = {0, 1, 2, 3, 7};
-    std::for_each(numeros.begin(), numeros.end(), [](int num) {
-        std::cout << num << std::endl;
+    for_each(numeros.begin(), numeros.end(), [&](int num) {
+        std::cout << "El cuadrado de: " << num << " es " << cuadrado(num) << std::endl;;
     });
 
 
