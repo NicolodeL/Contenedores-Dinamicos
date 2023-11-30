816d6106946cd8212087375a28579f4018c3fd92
NlodeL <nlodelop@myuax.com>
2023-11-30 15:35:10 +0100

    veo que ya se puede empezar a operar con el valarray al que llamé numeros, asi que hago primero una suma

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
-    valarray<double> va = {1.0, 2.0, 3.0, 4.0, 5.0};
+    valarray<double> numeros = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    cout << "Suma: " << numeros.sum() << std::endl;
 
 }
