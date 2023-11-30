99dedc2294815e7962c802f571185c2ea6a517ed
NlodeL <nlodelop@myuax.com>
2023-11-30 16:51:59 +0100

    mientras se instala la biblioteca voy añadiendo el operador try por si ocurre algun error interceptarlo más adelante

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+#include "lexical_cast.hpp"
 using namespace std;
 
 int main() {
 string codigo = "58192";
+try{
 int descodificado = boost::lexical_cast<int>(codigo);
 cout << "Número: " << descodificado << endl;
-
-
 }
+    }
