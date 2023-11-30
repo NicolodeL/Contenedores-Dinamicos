e3096aef6ec47cf1856c75268bd9d8795e3913bf
NlodeL <nlodelop@myuax.com>
2023-11-30 16:25:35 +0100

    Elimino la biblioteca ya no era la correcta

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include "lexical_cast.hpp"
+
 using namespace std;
 
 int main() {
 string codigo = "58192";
+int descodificado = boost::lexical_cast<int>(codigo);
+cout << "Número: " << descodificado << endl;
 
 
 }
