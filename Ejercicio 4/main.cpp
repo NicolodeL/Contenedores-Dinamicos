b20d996c5fad0884ff585c27118a9a775ba90f9f
NlodeL <nlodelop@myuax.com>
2023-11-30 15:43:58 +0100

    cambio el valarray numeros 2 para que su media de decimales, compruebo que si los da

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     valarray<double> numeros = {1.0, 2.0, 3.0, 4.0, 5.0};
-    valarray<double> numeros2 = {2.0, 4.0, 6.0, 8.0, 10.0};
+    valarray<double> numeros2 = {2.0, 4.0, 6.0, 8.0, 9.0};
     valarray<double> resultado = numeros - numeros2;
 
     cout << "Suma: " << numeros.sum() << endl;
@@ -14,6 +14,10 @@ int main() {
     }
 
     cout << "\nMedia: " << numeros.sum() / numeros.size() << endl;
+
+    cout << "\nMedia: " << numeros2.sum() / numeros2.size() << endl;
+
+
 
 
 }
