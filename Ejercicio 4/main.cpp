3a11b1717c183469dfd79445166c519674aa5d76
NlodeL <nlodelop@myuax.com>
2023-11-30 15:41:18 +0100

    creo otro valarray(numeros 2) para poder operaro con el 1, pruebo a restarlos

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main() {
     valarray<double> numeros = {1.0, 2.0, 3.0, 4.0, 5.0};
+    valarray<double> numeros2 = {2.0, 4.0, 6.0, 8.0, 10.0};
+    valarray<double> resultado = numeros - numeros2;
 
-    cout << "Suma: " << numeros.sum() << std::endl;
+    cout << "Suma: " << numeros.sum() << endl;
+
+    for (auto &v : resultado) {
+        cout << v << " ";
+    }
+    cout << endl;
+
 
 }
